c98e0753d7971579b90a265a18167010ceea4a99
[Ahad Ali] <[mdaliahad@outlook.com]>
2022-01-07 10:33:38 +0800

    mobile menu created

--- script.js
+++ script.js
@@ -10,6 +10,7 @@ const image5= document.getElementById('image5');
 const image6= document.getElementById('image6');
 const textBox= document.getElementById('text-box');
 
+
 // Dark or Light Image
 function imageMode(color){
     image1.src= `img/responsive-${color}.svg`;
@@ -61,6 +62,9 @@ function switchTheme(event){
 
 // eventlistener that will change event
 toggleSwitch.addEventListener('change', switchTheme);
+
+
+
 
 
 // Check Local Storage For Theme
